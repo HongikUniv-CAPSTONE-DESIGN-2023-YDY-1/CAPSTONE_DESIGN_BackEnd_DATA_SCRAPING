d71d75cd9776676035ce4240a1c47aeef67960bd
robinjoon <robin980108@naver.com>
2023-10-16 00:32:44 +0900

    Fix(정규식) : 정규식 오류 수정

--- src/main/java/com/example/demo/service/RegixSubCategoryService.java
+++ src/main/java/com/example/demo/service/RegixSubCategoryService.java
@@ -79,7 +79,7 @@ public class RegixSubCategoryService implements SubCategoryService {
         String candyRegex = "(?i)(캔디|사탕|롤리팝|쥬시드랍딥퍼즈|쥬시드랍팝캔디|카라멜|이클립스|마시멜로)";
         String crackerRegex = "(?i)(크래커|리츠|샌드|오레오|비스코프|깜드|빈츠|빠다코코낫|제크|쿠키)";
         String basicSnack = "(?i)(새우깡|치토스|꼬깔콘|나초|도리토스|칩|오징어집|바나나킥|포스틱)";
-        String cornSnack = "(?i)(크래커|리츠|샌드|오레오|비스코프|깜드|초코쿠키)";
+//        String cornSnack = "(?i)(크래커|리츠|샌드|오레오|비스코프|깜드|초코쿠키)";
         String hotSnack = "(?i)(매콤|핫|스파이시|매운|마라)";
         String boxSnack = "(?i)(초코파이|쿠쿠다스|마가렛트|카스타드|몽쉘|빅파이|오예스|후렌치파이|초코하임|화이트하임)";
         String potatoSnack = "(?i)(포테토칩|감자|스윙칩|프링글스)";
@@ -92,14 +92,14 @@ public class RegixSubCategoryService implements SubCategoryService {
         boolean isCandy = calcByRegex(candyRegex, "캔디", promotion);
         boolean isCracker = calcByRegex(crackerRegex, "크래커", promotion);
         boolean isBasicSnack = calcByRegex(basicSnack, "기본 스낵", promotion);
-        boolean isCornSnack = calcByRegex(cornSnack, "옥수수 스낵", promotion);
+//        boolean isCornSnack = calcByRegex(cornSnack, "옥수수 스낵", promotion);
         boolean isHotSnack = calcByRegex(hotSnack, "매운 스낵", promotion);
         boolean isBoxSnack = calcByRegex(boxSnack, "박스 스낵", promotion);
         boolean isPotatoSnack = calcByRegex(potatoSnack, "감자 스낵", promotion);
         boolean isKidsSnack = calcByRegex(kidsSnack, "어린이 스낵", promotion);
 
         if (!isJelly && !isEnergyBar && !isProtein && !isChoco && !isCandy && !isCracker
-                && !isBasicSnack && !isCornSnack && !isHotSnack && !isBoxSnack && !isPotatoSnack && !isKidsSnack) {
+                && !isBasicSnack && !isHotSnack && !isBoxSnack && !isPotatoSnack && !isKidsSnack) {
             promotion.addSubCategory("기본 스낵", 1);
         }
     }
@@ -107,23 +107,23 @@ public class RegixSubCategoryService implements SubCategoryService {
     public void calcFOODSubCategory(Promotion promotion) {
         // 각각의 정규식 패턴과 boolean 변수를 설정합니다.
         String cupBobRegex = "(?i)(컵밥|컵반|국밥|볶음밥)";
-        String iceFoodRegex = "(?!)(만두|교자|피자|닭강정|냉동|치킨|슈마이)";
-        String sausageRegex = "(?!)(후랑크|프랑크|핫바|꼬치바|소시지|맥스봉|비엔나|꼬치다|스노우크랩킹" +
+        String iceFoodRegex = "(?i)(만두|교자|피자|닭강정|냉동|치킨|슈마이)";
+        String sausageRegex = "(?i)(후랑크|프랑크|핫바|꼬치바|소시지|맥스봉|비엔나|꼬치다|스노우크랩킹" +
                 "|오븐구이치킨봉|오븐구이치킨윙|휠터치|크랩|직꾸닭|핫도그|육즙가득|부리또|천하장사)";
         String canFoodRegex = "(?i)(스팸싱글|스팸라이트|스팸클래식|리챔|살코기참치|동원참치|고추참치|야채참치|김치찌개참치)";
-        String soupRegex = "(?!)(수프|죽)";
-        String healthyFoodRegex = "(?!)(그레인보울|프로틴|닭가슴살|두부|건강|레모나|비타민|치즈|쓹닭|정관장)";
-        String riceRegex = "(?!)(햇반|오뚜기밥|흑미밥|잡곡밥|백미밥|현미밥|고시히카리밥|영양밥|약밥|맛있는큰밥|오곡밥)";
-        String ramenRegex = "(?!)(라면|너구리|안성탕면|불닭볶음면|짜파게티|마라탕면|무파마|비빔면|배홍동|사리곰탕면|오징어짬뽕" +
+        String soupRegex = "(?i)(수프|죽)";
+        String healthyFoodRegex = "(?i)(그레인보울|프로틴|닭가슴살|두부|건강|레모나|비타민|치즈|쓹닭|정관장)";
+        String riceRegex = "(?i)(햇반|오뚜기밥|흑미밥|잡곡밥|백미밥|현미밥|고시히카리밥|영양밥|약밥|맛있는큰밥|오곡밥)";
+        String ramenRegex = "(?i)(라면|너구리|안성탕면|불닭볶음면|짜파게티|마라탕면|무파마|비빔면|배홍동|사리곰탕면|오징어짬뽕" +
                 "|진짬뽕|카구리|짜파구리|컵누들|컵쌀국수|큰컵|사발|큰사발|야끼소바불닭|쿠티크투움바파스타|쿠티크매콤물비빔면|짜슐랭" +
                 "|치즈게티|우육면|세이면)";
-        String tangRegex = "(?!)(찌개|곰탕|설렁탕|삼계탕|추어탕|미역국|무국|장터국|순대국|순댓국|된장국|어묵탕|짜글이)";
-        String kimchiRegex = "(?!)(김치|젓갈)";
+        String tangRegex = "(?i)(찌개|곰탕|설렁탕|삼계탕|추어탕|미역국|무국|장터국|순대국|순댓국|된장국|어묵탕|짜글이)";
+        String kimchiRegex = "(?i)(김치|젓갈)";
         String jellyRegex = "(?i)(젤리|쁘띠첼|구미|해피세븐|하리보|탱글탱글)";
         String noodleRegex = "(?i)(촙촙면|스파게티|인천식옛날짜장|장칼국수|하이면|로제불닭떡볶이|납작당면|떡볶이|라뽂이|파스타|사천짜복이컵" +
                 "|막국수|김치말이국수)";
-        String withDrinkRegex = "(?i)(아몬드|믹스넛|노가리|먹태|육포|어묵탕|포차|족발|머릿|안주|곱창|상상비프|땅콩|무침||소금구이|직화구이|닭발)";
-        String hangOverRegex = "(?!)(상쾌환|오징어|케이쿨환|비상대책환|한잔허제간만세|헛깨하니술깨|숙취)";
+        String withDrinkRegex = "(?i)(아몬드|믹스넛|노가리|먹태|육포|어묵탕|포차|족발|머릿|안주|곱창|상상비프|땅콩|무침|소금구이|직화구이|닭발)";
+        String hangOverRegex = "(?i)(상쾌환|오징어|케이쿨환|비상대책환|한잔허제간만세|헛깨하니술깨|숙취)";
         String breadRegex = "(?i)(브레디크|크림까눌레|꿀호떡|샌드|와플|요팡|마팡|타이야키)";
         String curryRegex = "(?i)(커리|카레)";
         String cerealRegex = "(?i)(콘푸로스트|시리얼|첵스초코|그래놀라컵|후레이크|크랜베리그래놀라)";
